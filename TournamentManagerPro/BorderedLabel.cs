15d58fb442e9d1a6f26309e6ee5d340fe7c878de
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-05-17 21:04:00 +0300

    Нова функционалност - Процес на разработка

---
+++ TournamentManagerPro/BorderedLabel.cs
@@ -0,0 +1,50 @@
+﻿/**
+ * Tournament Manager Pro
+ * 
+ * BorderedLabel - Special label with borders
+ */
+
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TournamentManagerPro
+{
+
+    internal class BorderedLabel : Label
+    {
+
+        //The border color - by default is black
+        public Color BorderColor { get; set; } = Color.Black;
+
+        //The tickness of the border - by default is 1px
+        public int BorderThickness { get; set; } = 1;
+
+
+        /*
+         * The contructor of the label 
+         */
+        public BorderedLabel()
+        {
+            AutoSize = false;
+        }
+
+
+        /*
+         * Overrides OnPaint method 
+         */
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            base.OnPaint(e);
+
+            using (Pen writer = new Pen(BorderColor, BorderThickness))
+            {
+                Rectangle button = new Rectangle(0, 0, Width - 1, Height - 1);
+                e.Graphics.DrawRectangle(writer, button);
+            }
+
+        }
+
+    }
+
+}
--- TournamentManagerPro/ManagerGUI.cs
+++ TournamentManagerPro/ManagerGUI.cs
@@ -6,7 +6,7 @@
 
 using System.Windows.Forms;
 using System.Drawing;
-using System.Runtime.Versioning;
+
 
 namespace TournamentManagerPro
 {
@@ -24,16 +24,16 @@ namespace TournamentManagerPro
         private PictureBox info;
 
         //UEFA Champions League
-        private Label UefaChampionsLeague;
+        private BorderedLabel UefaChampionsLeague;
 
         //UEFA Europe League
-        private Label UefaEuropeLeague;
+        private BorderedLabel UefaEuropeLeague;
 
         //FIFA World Cup
-        private Label FifaWorldCup;
+        private BorderedLabel FifaWorldCup;
 
         //UEFA Euro
-        private Label UefaEuro;
+        private BorderedLabel UefaEuro;
 
         //ToolTip for the labels/buttons
         private ToolTip tip = new ToolTip();
@@ -50,8 +50,8 @@ namespace TournamentManagerPro
 
             //Fix the window
             Text = "Pro Evolution Soccer 2013 BUL MOD 7 Manager Pro";
-            Icon = new Icon("C:\\Users\\Cvete\\Documents\\Visual Studio 2022\\Projects\\Tournament Manager Pro\\ProjectTournamentManagerPro\\TournamentManagerPro\\Resources\\icon.ico");
-            Size = new Size(1200, 650);
+            Icon = Properties.Resources.icon;
+            Size = new Size(1055, 650);
             BackColor = Color.GhostWhite;
             StartPosition = FormStartPosition.CenterScreen;
             MaximizeBox = false;
@@ -71,8 +71,8 @@ namespace TournamentManagerPro
             //Manager logo
             pesManagerLogo = new PictureBox()
             {
-                Bounds = new Rectangle(0, 0, 1200, 350),
-                Image = Image.FromFile("C:\\Users\\Cvete\\Documents\\Visual Studio 2022\\Projects\\Tournament Manager Pro\\ProjectTournamentManagerPro\\TournamentManagerPro\\Resources\\manager.jpg"),
+                Bounds = new Rectangle(0, 0, 1039, 350),
+                Image = Properties.Resources.manager,
                 SizeMode = PictureBoxSizeMode.StretchImage
             };
             Controls.Add(pesManagerLogo);
@@ -81,7 +81,7 @@ namespace TournamentManagerPro
             bottomBorder = new PictureBox()
             {
                 Bounds = new Rectangle(0, 595, 1200, 14),
-                Image = Image.FromFile("C:\\Users\\Cvete\\Documents\\Visual Studio 2022\\Projects\\Tournament Manager Pro\\ProjectTournamentManagerPro\\TournamentManagerPro\\Resources\\bottomImage.jpg"),
+                Image = Properties.Resources.Pes2013,
                 SizeMode = PictureBoxSizeMode.StretchImage
             };
             Controls.Add(bottomBorder);
@@ -89,12 +89,112 @@ namespace TournamentManagerPro
             //Information 
             info = new PictureBox()
             {
-                Bounds = new Rectangle(10, 370, 25, 25),
-                Image = Image.FromFile("C:\\Users\\Cvete\\Documents\\Visual Studio 2022\\Projects\\Tournament Manager Pro\\ProjectTournamentManagerPro\\TournamentManagerPro\\Resources\\information.png"),
+                Bounds = new Rectangle(7, 355, 25, 25),
+                Image = Properties.Resources.info,
                 SizeMode = PictureBoxSizeMode.StretchImage
             };
             tip.SetToolTip(info, "Избери турнир и следвай стъпките на мениджъра");
             Controls.Add(info);
+
+            //UEFA Champions League
+            UefaChampionsLeague = new BorderedLabel()
+            {
+                Text = "UEFA Champions League",
+                Font = new Font("Cascadia Code", 16, FontStyle.Italic),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Bounds = new Rectangle(70, 400, 450, 80),
+                ForeColor = Color.FromArgb(60, 110, 110, 110),
+                BorderColor = Color.FromArgb(60, 110, 110, 110)
+            };
+            UefaChampionsLeague.MouseEnter += (sender, eventArgs) =>
+            {
+                UefaChampionsLeague.ForeColor = Color.Black;
+                UefaChampionsLeague.BorderColor = Color.DarkBlue;
+                UefaChampionsLeague.BackColor = Color.FromArgb(10, 110, 110, 250);
+            };
+            UefaChampionsLeague.MouseLeave += (sender, eventArgs) =>
+            {
+                UefaChampionsLeague.ForeColor = Color.FromArgb(60, 110, 110, 110);
+                UefaChampionsLeague.BorderColor = Color.FromArgb(60, 110, 110, 110);
+                UefaChampionsLeague.BackColor = Color.GhostWhite;
+            };
+            tip.SetToolTip(UefaChampionsLeague, "Разпредели отборите от Шампионска Лига");
+            Controls.Add(UefaChampionsLeague);
+
+            //UEFA Europe League
+            UefaEuropeLeague = new BorderedLabel()
+            {
+                Text = "UEFA Europe League",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = UefaChampionsLeague.Font,
+                ForeColor = UefaChampionsLeague.ForeColor,
+                BorderColor = UefaChampionsLeague.BorderColor,
+                Bounds = new Rectangle(521, 400, 450, 80)
+            };
+            UefaEuropeLeague.MouseEnter += (sender, eventArgs) =>
+            {
+                UefaEuropeLeague.ForeColor = Color.Black;
+                UefaEuropeLeague.BorderColor = Color.DarkBlue;
+                UefaEuropeLeague.BackColor = Color.FromArgb(10, 110, 110, 250);
+            };
+            UefaEuropeLeague.MouseLeave += (sender, eventArgs) =>
+            {
+                UefaEuropeLeague.ForeColor = Color.FromArgb(60, 110, 110, 110);
+                UefaEuropeLeague.BorderColor = Color.FromArgb(60, 110, 110, 110);
+                UefaEuropeLeague.BackColor = Color.GhostWhite;
+            };
+            tip.SetToolTip(UefaEuropeLeague, "Разпредели отборите от Лига Европа");
+            Controls.Add(UefaEuropeLeague);
+
+            //UEFA Euro France
+            UefaEuro = new BorderedLabel()
+            {
+                Text = "UEFA Euro France",
+                Font = UefaChampionsLeague.Font,
+                TextAlign = ContentAlignment.MiddleCenter,
+                ForeColor = UefaChampionsLeague.ForeColor,
+                BorderColor = UefaChampionsLeague.BorderColor,
+                Bounds = new Rectangle(70, 481, 450, 80)
+            };
+            UefaEuro.MouseEnter += (sender, eventArgs) =>
+            {
+                UefaEuro.ForeColor = Color.Black;
+                UefaEuro.BorderColor = Color.DarkBlue;
+                UefaEuro.BackColor = Color.FromArgb(10, 110, 110, 250);
+            };
+            UefaEuro.MouseLeave += (sender, eventArgs) =>
+            {
+                UefaEuro.ForeColor = Color.FromArgb(60, 110, 110, 110);
+                UefaEuro.BorderColor = Color.FromArgb(60, 110, 110, 110);
+                UefaEuro.BackColor = Color.GhostWhite;
+            };
+            tip.SetToolTip(UefaEuro, "Разпредели отборите от Европейското футболно първенство");
+            Controls.Add(UefaEuro);
+
+            //Fifa World Cup
+            FifaWorldCup = new BorderedLabel()
+            {
+                Text = "FIFA World Cup",
+                Font = UefaChampionsLeague.Font,
+                TextAlign = ContentAlignment.MiddleCenter,
+                ForeColor = UefaChampionsLeague.ForeColor,
+                BorderColor = UefaChampionsLeague.BorderColor,
+                Bounds = new Rectangle(521, 481, 450, 80)
+            };
+            FifaWorldCup.MouseEnter += (s, e) =>
+            {
+                FifaWorldCup.ForeColor = Color.Black;
+                FifaWorldCup.BorderColor = Color.DarkBlue;
+                FifaWorldCup.BackColor = Color.FromArgb(10, 110, 110, 250);
+            };
+            FifaWorldCup.MouseLeave += (s, e) =>
+            {
+                FifaWorldCup.ForeColor = Color.FromArgb(60, 110, 110, 110);
+                FifaWorldCup.BorderColor = Color.FromArgb(60, 110, 110, 110);
+                FifaWorldCup.BackColor = Color.GhostWhite;
+            };
+            tip.SetToolTip(FifaWorldCup, "Разпредели отборите от Световното първенство по футбол");
+            Controls.Add(FifaWorldCup);
 
         }
 
